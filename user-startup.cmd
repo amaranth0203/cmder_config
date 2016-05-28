a97242ef4301100685b80f4f1953e8e264e5d6cd
qiyunhu <qiyunhu@bbktel.com>
2016-05-28 10:27:05 +0800

    init

---
+++ user-startup.cmd
@@ -0,0 +1,10 @@
+:: use this file to run your own startup commands 
+:: use @ in front of the command to prevent printing the command
+ 
+:: @call "C:\Users\Administrator.AXX3DX8ZNMSGU5B\Desktop\cmder\vendor\git-for-windows/cmd/start-ssh-agent.cmd
+:: @set PATH=%CMDER_ROOT%\vendor\whatever;%PATH%
+ 
+@set wv=%home%\.qyh_vim\
+@set ws=E:\Sources
+@set wn=E:\notes
+@call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
